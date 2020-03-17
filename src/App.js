5cf4aa96a28170f4cc8ef43df1ba5c75ee31620b
John Guinn <jguinn124@gmail.com>
2020-03-16 20:34:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props){
+	super(props);
+
+	this.state={
+		newItem:"",
+		list: []
+	}
+
+	addItem(){
+		//create item with unique id
+		const newItem={
+			id: 1 + Math.random(),
+			value: this.state.newItem.slice()
+		};
+
+		//copy of current list of items
+		const list = [...this.state.list];
+
+		//add new item to list
+	}
+	render() {
+		return(
+			<div className="App">
+				<div>
+					Add an Item
+					<br>
+					<input
+					type="text"
+					placeholder="Type item here"
+					value={this.state.newItem}
+					onChange={e => this.updateInput("newItem", e.target.value)}
+					/>
+					<button
+					onClick={() => this.addItem()}
+					>
+						Add
+					</button>
+				</div>
+
+			</div>
+		);
+	}
+
+  }
+    
 
 export default App;
